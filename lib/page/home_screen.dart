6f8498a0a2f8762e11c8835f639cbba835bae56a
junyongcho1 <chojun2166@naver.com>
2023-05-01 17:57:27 +0900

    bottom navigation bar 확정

--- lib/page/home_screen.dart
+++ lib/page/home_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:yong_project/logout/signout.dart';
-import 'package:yong_project/page/new_screen.dart';
-import 'package:yong_project/services/google_service.dart';
-import 'package:yong_project/services/kakao_login.dart';
-import 'package:yong_project/services/kakao_main_view.dart';
+//import 'package:flutter/src/widgets/framework.dart';
+//import 'package:flutter/src/widgets/placeholder.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:google_sign_in/google_sign_in.dart';
+//import 'package:yong_project/logout/signout.dart';
+//import 'package:yong_project/page/new_screen.dart';
+import 'package:yong_project/screen/first_screen.dart';
+import 'package:yong_project/screen/second_screen.dart';
+import 'package:yong_project/screen/thrid_screen.dart';
+//import 'package:yong_project/services/google_service.dart';
+// import 'package:yong_project/services/kakao_login.dart';
+// import 'package:yong_project/services/kakao_main_view.dart';
 import 'package:yong_project/startpage.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -18,87 +21,61 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final viewModel = MAinViewModel(KakaoLoginUp());
+  //final viewModel = MAinViewModel(KakaoLoginUp());
+  int currentIndex = 0;
+  final screens = [
+    FirstScreen(),
+    SecondScreen(),
+    ThirdScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser!;
-    Signout _signout = Signout();
+    // final user = FirebaseAuth.instance.currentUser!;
+    // Signout _signout = Signout();
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('로그인 됨'),
-            SizedBox(
-              height: 8,
-            ),
-            // CircleAvatar(
-            //   radius: 40,
-            //   backgroundImage: NetworkImage(user.photoURL!),
-            // ),
-            // SizedBox(
-            //   height: 8,
-            // ),
-            // Text(
-            //   '이름: ' + user.displayName!,
-            //   style: TextStyle(color: Colors.white, fontSize: 16),
-            // ),
-            // SizedBox(
-            //   height: 8,
-            // ),
-            Text(
-              '이메일: ' + user.email!,
-              style: TextStyle(color: Colors.black, fontSize: 16),
-            ),
-            Text(
-              '이름: ' + user.displayName!,
-              style: TextStyle(color: Colors.black, fontSize: 16),
-            ),
-
-            SizedBox(
-              height: 8,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  _signout.signout();
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => StartPage()));
-                  // signOut();
-                },
-                child: Text('real 로그아웃!!'))
-          ],
-        ),
-      ),
+      body: screens[currentIndex],
+      ////////////bottom navigation bar////////////////////
       bottomNavigationBar: BottomNavigationBar(
-        onTap: (value) {
-          switch (value) {
-            case 0:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => NewScreen()),
-              );
-              break;
-            case 1:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => NewScreen()),
-              );
-              break;
-            case 2:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => NewScreen()),
-              );
-              break;
-          }
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Color.fromARGB(255, 71, 233, 133),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white70,
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            print(index);
+            currentIndex = index;
+          });
+          // print(index);
+          // switch (index) {
+          //   case 0:
+          //     currentIndex;
+          //     // Navigator.push(
+          //     //   context,
+          //     //   MaterialPageRoute(builder: (context) => HomeScreen()),
+          //     // );
+          //     break;
+          //   case 1:
+          //     currentIndex;
+          //     // Navigator.push(
+          //     //   context,
+          //     //   MaterialPageRoute(builder: (context) => NewScreen()),
+          //     // );
+          //     break;
+          //   case 2:
+          //     currentIndex;
+          //     // Navigator.push(
+          //     //   context,
+          //     //   MaterialPageRoute(builder: (context) => NewScreen()),
+          //     // );
+          //     break;
+          // }
         },
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'add'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'profile'),
           BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'Menu'),
         ],
-        selectedItemColor: Color.fromARGB(255, 71, 233, 133),
       ),
     );
   }
---
+++ lib/screen/thrid_screen.dart
@@ -0,0 +1,66 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:yong_project/logout/signout.dart';
+import 'package:yong_project/startpage.dart';
+
+class ThirdScreen extends StatefulWidget {
+  const ThirdScreen({super.key});
+
+  @override
+  State<ThirdScreen> createState() => _ThirdScreenState();
+}
+
+class _ThirdScreenState extends State<ThirdScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser!;
+    Signout _signout = Signout();
+    return Scaffold(
+        body: Padding(
+      padding: EdgeInsets.all(32),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('로그인 됨'),
+          SizedBox(
+            height: 8,
+          ),
+          // CircleAvatar(
+          //   radius: 40,
+          //   backgroundImage: NetworkImage(user.photoURL!),
+          // ),
+          // SizedBox(
+          //   height: 8,
+          // ),
+          // Text(
+          //   '이름: ' + user.displayName!,
+          //   style: TextStyle(color: Colors.white, fontSize: 16),
+          // ),
+          // SizedBox(
+          //   height: 8,
+          // ),
+          Text(
+            '이메일: ' + user.email!,
+            style: TextStyle(color: Colors.black, fontSize: 16),
+          ),
+          Text(
+            '이름: ' + user.displayName!,
+            style: TextStyle(color: Colors.black, fontSize: 16),
+          ),
+
+          SizedBox(
+            height: 8,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                _signout.signout();
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => StartPage()));
+                // signOut();
+              },
+              child: Text('real 로그아웃!!'))
+        ],
+      ),
+    ));
+  }
+}
